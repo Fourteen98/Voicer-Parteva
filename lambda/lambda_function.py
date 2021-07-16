3de7340bd17afe0e1ebaa7debdcbc1bf95bb862f
Abdul Basit Mohammed <abdulbasitm810@gmail.com>
2021-07-16 04:39:40 +0000

    added Ktumotto intent and it handler. also moved all the custom intent handler to the right position so that intentreflectorhandler is the last

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -251,7 +251,23 @@ class KtuEstablishedIntentHandler(AbstractRequestHandler):
                 .response
         )
 
+class KtuMottoIntentHandler(AbstractRequestHandler):
+    
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("KtuMottoIntent")(handler_input)
 
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "The motto of Koforidua Technical University is Innovating for development"
+        
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
@@ -260,13 +276,16 @@ sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
 #KtuKeyInfo handlers
 sb.add_request_handler(KtuWebsiteIntentHandler())
 sb.add_request_handler(KtuSrcPresidentIntentHandler())
 sb.add_request_handler(KtuViceChancellorIntentHandler())
 sb.add_request_handler(KtuEstablishedIntentHandler())
+sb.add_request_handler(KtuMottoIntentHandler())
+
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
 
 sb.add_exception_handler(CatchAllExceptionHandler())
 
